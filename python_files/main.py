431996edf4f8547a7dd60ff9bb0a48e859211a76
INS140 <IsaacNSaettel@gmail.com>
2022-10-11 00:22:25 -0700

    main

--- python_files/main.py
+++ python_files/main.py
@@ -1,28 +1,30 @@
 import Monster_SB
 
-# Hello! If you're reading this then you probably clicked on the link to see my combat simulator!
-# This is a D&D oriented simulator, so it uses a lot of the vocabulary from that format.
-# You'll need to copy all three files to play the game.
+'''
+Hello! If you're reading this then you probably clicked on the link to see my combat simulator!
+This is a D&D oriented simulator, so it uses a lot of the vocabulary from that format.
+You'll need to copy all three files to play the game.
 
-# HOW TO PLAY:
-# 1. Set number of players. You can set as many as you like, but I recommend no more than 5.
-#    Setting the number to 1 will stop the game early.
-# 2. Set player types (available types include: player, gnoll, skeleton, and zombie. More to be added soon!).
-#    It is not recommended to set combatants to "player" unless you understand how D&D combat works.
-#    My advice, stick with the zombies. Makes it way easier.
-# 3. If you select "player" as the player type, you'll need to provide an HP and AC, see vocab reference for details.
-#    The game presets these stats for "monster" types.
-# 4. For both monsters and players, you'll need to set a name. Names will be used in a couple different ways.
-# 5. When all your combatants are set up, the game will start.
-# 6. The game is a free-for-all and will only end when there is one combatant left.
-# 7. At the beginning of each turn you will be asked to type a command. Type "help" for a list of commands.
-# 8. Once there is only one combatant left standing, the win message will appear with that combatant's name.
-# 9. Enjoy!
+HOW TO PLAY:
+1. Set number of players. You can set as many as you like, but I recommend no more than 5.
+   Setting the number to 1 will stop the game early.
+2. Set player types (available types include: player, gnoll, skeleton, and zombie. More to be added soon!).
+   It is not recommended to set combatants to "player" unless you understand how D&D combat works.
+   My advice, stick with the zombies. Makes it way easier.
+3. If you select "player" as the player type, you'll need to provide an HP and AC, see vocab reference for details.
+   The game presets these stats for "monster" types.
+4. For both monsters and players, you'll need to set a name. Names will be used in a couple different ways.
+5. When all your combatants are set up, the game will start.
+6. The game is a free-for-all and will only end when there is one combatant left.
+7. At the beginning of each turn you will be asked to type a command. Type "help" for a list of commands.
+8. Once there is only one combatant left standing, the win message will appear with that combatant's name.
+9. Enjoy!
 
-# Vocab reference:
-# HP = hit points !!!RECOMMENDED 30 OR BELOW!!!
-# AC = Armor class, value in D&D that determines defense. !!!MUST BE A VALUE BETWEEN 1-20!!!
-# Initiative = Value that determines combatant turn order
+Vocab reference:
+HP = hit points !!!RECOMMENDED 30 OR BELOW!!!
+AC = Armor class, value in D&D that determines defense. !!!MUST BE A VALUE BETWEEN 1-20!!!
+Initiative = Value that determines combatant turn order
+'''
 
 # This is still a work in progress, but I hope to eventually develop this into a functional app.
 
@@ -139,14 +141,20 @@ Round {round_num}''')
                             print('''Type a combatants name to target it
 Names can be found in "Player Names" list at beginning of Round''')
                             target = input('target > ')
+                        elif target == 'cancel':
+                            player.command()
+                            break
                         elif target == 0:
                             pass
                         else:
                             print('''Invalid target
 Type "help" for options''')
                             target = input('target > ')
-                    turn_finished = True
-                    print('-' * 100)
+                    if attack_attempt is False:
+                        pass
+                    elif attack_attempt is True:
+                        turn_finished = True
+                        print('-' * 100)
                 elif player.com == 'def': # The Defend Command
                     player.defend()
                     turn_finished = True
